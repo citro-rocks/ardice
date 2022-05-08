c652f25fecefbacabea7b301f0cba4815a2750c1
Nikola Naskov <nnaskov@gmail.com>
2022-05-08 16:58:59 +0200

    init

--- da-re/src/components/PersonCard.js
+++ da-re/src/components/PersonCard.js
@@ -2,35 +2,38 @@ import React from 'react'
 // CSS
 import styles from "./PersonCard.module.css";
 
-// JSON 
-// import member from "./inovators/Inovators.json";
+import inovators from '../inovators/Inovators';
 
 const PersonCard = () => {
+    console.log(inovators);
+    const listItems = inovators.map((item) => 
+        <div className={styles.container}>
+            <div className={styles.grid}>
+                <div className={styles.card}  key={item.id}>
+                    <div className={styles.person_img}>
+                        <img src={item.image} alt="" />
+                    </div>
+                    <div className={styles.person_name}>
+                            <h2>{item.name}</h2>
+                            <p>{item.title}</p>
+                        </div>
+                    <div className={styles.description}>
+                        <p>{item.description}</p>
+                    </div>
+                    <div className={styles.social_links}>
+                        <ul>
+                            <li>{item.ln}</li>
+                            <li>{item.mail}</li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
 
   return (
     <div>
-      <div className={styles.container}>
-          <div className={styles.grid}>
-              <div className={styles.card}>
-                  <div className={styles.person_img}>
-                      <img src="" alt="" />
-                      <div className={styles.person_name}>
-                        <h2>Name</h2>
-                        <p>Title</p>
-                      </div>
-                  </div>
-                  <div className={styles.description}>
-                      <p>Description</p>
-                  </div>
-                  <div className={styles.social_links}>
-                      <ul>
-                          <li>twitter</li>
-                          <li>mail</li>
-                      </ul>
-                  </div>
-              </div>
-          </div>
-      </div>
+      {listItems}
     </div>
   )
 }
---
+++ da-re/src/inovators/Inovators.js
@@ -0,0 +1,84 @@
+const inovators = [{
+    id: 1,
+    image: "https://darehealthinnovation.nl/wp-content/uploads/2020/08/reinier-h2.jpg",
+    name: "Reinier Hakvoort",
+    title: "CO-Founder",
+    description: "Co-founder Reinier Hakvoort begon ooit zijn carrière bij Defensie, maar zette uiteindelijk in 2015 zijn eerste stap binnen bij een grote zorgorganisatie. En dat was een treffer. Reinier volgde de opleidingen Digitale Transformatie en Strategy & Leadership en ontdekte zijn passie voor technologie. In zijn vrije tijd vind je hem vaak op Ameland of in zijn wieleroutfit.",
+    ln: "https://linkedin.com/",
+    mail: "https://mail.google.com/"
+},
+{
+    id: 2,
+    image: "https://media-exp1.licdn.com/dms/image/C5603AQEYIRw0gskQKQ/profile-displayphoto-shrink_800_800/0/1591789172670?e=1657756800&v=beta&t=E9jkZzFV2doldeTPYjF4DkUe_oOtUEkVC3f2z-DaiZo",
+    name: "David de Jong",
+    title: "CO-Founder",
+    description: "Co-founder David de Jong is één van de meesterbreins achter DA-RE. Na zijn masterstudie Health Innovation, wist hij direct wat-ie wilde: de zorgwereld vooruit helpen. David ontdekt niet alleen de vele technologische mogelijkheden binnen de zorg, maar ook graag de wereld. Reizen is één van z’n passies. Maar is hij in Nederland, dan gaat David graag de strijd aan op de tennisbaan.",
+    ln: "https://linkedin.com/",
+    mail: "https://mail.google.com/"
+},
+{
+    id: 3,
+    image: "https://media-exp1.licdn.com/dms/image/C4E03AQGyrgVglq8Y1g/profile-displayphoto-shrink_800_800/0/1603213100859?e=1657756800&v=beta&t=hLGV5XoK_1a7eqNVVRWF-Sl_Fx713uehRZHEGAWnhgA",
+    name: "Arnold Boekema",
+    title: "Business Developer",
+    description: "Business developer Arnold heeft met zijn 20 jaar ervaring in verschillende branches en functies de juiste skills op zak om DA-RE te ondersteunen bij de ontwikkeling van ARNA, ons digitaal informatieplatform. Arnold houdt van aanpakken; bedrijven en mensen (letterlijk) in beweging brengen. Hij is actief betrokken bij verschillende sportevenementen en -verenigingen in zijn woonplaats Marum. Wanneer Arnold zich niet op het voetbalveld bevindt, dan geniet hij van een mooie rit door de natuur op zijn racefiets of mountainbike.",
+    ln: "https://linkedin.com/",
+    mail: "https://mail.google.com/"
+},
+{
+    id: 4,
+    image: "",
+    name: "",
+    title: "",
+    description: "",
+    ln: "",
+    mail: ""
+},
+{
+    id: 5,
+    image: "",
+    name: "",
+    title: "",
+    description: "",
+    ln: "",
+    mail: ""
+},
+{
+    id: 6,
+    image: "",
+    name: "",
+    title: "",
+    description: "",
+    ln: "",
+    mail: ""
+},
+{
+    id: 7,
+    image: "",
+    name: "",
+    title: "",
+    description: "",
+    ln: "",
+    mail: ""
+},
+{
+    id: 8,
+    image: "",
+    name: "",
+    title: "",
+    description: "",
+    ln: "",
+    mail: ""
+},
+{
+    id: 9,
+    image: "",
+    name: "",
+    title: "",
+    description: "",
+    ln: "",
+    mail: ""
+}
+]
+
+export default inovators;
