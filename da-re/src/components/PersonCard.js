554c9b45258b7290f19d8779350b1a64a7f6a33f
Nikola Naskov <nnaskov@gmail.com>
2022-05-09 22:15:05 +0200

    init

--- da-re/src/components/PersonCard.js
+++ da-re/src/components/PersonCard.js
@@ -7,17 +7,17 @@ import inovators from '../inovators/Inovators_db';
 const PersonCard = () => {
     console.log(inovators);
     const listItems = inovators.map((item) => 
-        <div className={styles.contaner} key={item.id}>
-            <div className={styles.card}>
+        <div>
+            <div className={styles.card}  key={item.id}>
                 <div className={styles.card_img}>
                     <img src={item.image} alt="" />
-                </div>
-                <div className={styles.person_name}>
-                    <h2>{item.name}</h2>
-                    <p>{item.title}</p>
-                </div>
-                <div className={styles.description}>
-                    <p>{item.description}</p>
+                    <div className={styles.person_name}>
+                        <h2>{item.name}</h2>
+                        <p>{item.title}</p>
+                    </div>
+                    <div className={styles.description}>
+                        <p>{item.description}</p>
+                    </div>
                 </div>
                 <div className={styles.social_links}>
                     <ul>
@@ -30,7 +30,7 @@ const PersonCard = () => {
     );
 
   return (
-    <div>
+    <div  className={styles.contaner}>
       {listItems}
     </div>
   )
